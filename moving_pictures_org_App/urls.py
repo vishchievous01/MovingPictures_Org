d81781e13d3279bc46f3df34243163b948c219d7
vishchievous <vishnupv382001@gmail.com>
2025-05-18 21:24:37 +0530

    progress

--- moving_pictures_org_App/urls.py
+++ moving_pictures_org_App/urls.py
@@ -1,9 +1,26 @@
 from django.urls import path
 from moving_pictures_org.urls import views
 
-
 urlpatterns = [
+    path('index/', views.index, name="index"),
     path('welcome/', views.welcome, name="welcome"),
     path('errorPage/', views.errorPage, name="errorPage"),
     path('blogDetail/', views.blogDetail, name="blogDetail"),
+    path('blogGrid/', views.blogGrid, name="blogGrid"),
+    path('blogList.', views.blogList, name="blogList"),
+    path('celebrityGrid01/', views.celebrityGrid01, name="celebrityGrid01"),
+    path('celebrityGrid02/', views.celebrityGrid02, name="celebrityGrid02"),
+    path('celebrityList/', views.celebrityList, name="celebrityList"),
+    path('celebritySingle/', views.celebritySingle, name="celebritySingle"),
+    path('comingSoon/', views.comingSoon, name="comingSoon"),
+    path('homeV2/', views.homeV2, name="homeV2"),
+    path('homeV3/', views.homeV3, name="homeV3"),
+    path('movieGrid/', views.movieGrid, name="movieGrid"),
+    path('movieGridFW/', views.movieGridFW, name="movieGridFW"),
+    path('movieList/', views.movieList, name="movieList"),
+    path('movieSingle/', views.movieSingle, name="movieSingle"),
+    path('seriesSingle/', views.seriesSingle, name="seriesSingle"),
+    path('userFavoriteGrid/', views.userFavoriteGrid, name="userFavoriteGrid"),
+    path('userProfile/', views.userProfile, name="userProfile"),
+    path('userRate/', views.userRate, name="userRate"),
 ]
--- moving_pictures_org_App/views.py
+++ moving_pictures_org_App/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
+def index(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "index_light.html")
+    return render(request, "index.html")
+
+
+def index2(request):
+    return render(request, "index-2.html")
+
+
 def welcome(request):
     return render(request, "landing.html")
 
@@ -15,3 +26,119 @@ def blogDetail(request):
     if theme == 'light':
         return render(request, "blogdetail_light.html")
     return render(request, "blogdetail.html")
+
+
+def blogGrid(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "bloggrid_light.html")
+    return render(request, 'bloggrid.html')
+
+
+def blogList(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "bloglist_light.html")
+    return render(request, "bloglist.html")
+
+
+def celebrityGrid01(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "celebritygrid01_light.html")
+    return render(request, "celebritygrid01.html")
+
+
+def celebrityGrid02(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "celebritygrid02_light.html")
+    return render(request, "celebritygrid02.html")
+
+
+def celebrityList(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "celebritylist_light.html")
+    return render(request, "celebritylist.html")
+
+
+def celebritySingle(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "celebritysingle_light.html")
+    return render(request, "celebritysingle.html")
+
+
+def comingSoon(request):
+    return render(request, "comingsoon.html")
+
+
+def homeV2(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "homev2_light.html")
+    return render(request, "homev2.html")
+
+
+def homeV3(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "homev3_light.html")
+    return render(request, "homev3.html")
+
+
+def movieGrid(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "moviegrid_light.html")
+    return render(request, "moviegrid.html")
+
+
+def movieGridFW(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "moviegridfw_light.html")
+    return render(request, "moviegridfw.html")
+
+
+def movieList(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "movielist_light.html")
+    return render(request, "movielist.html")
+
+
+def movieSingle(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "moviesingle_light.html")
+    return render(request, "moviesingle.html")
+
+
+def seriesSingle(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "seriessingle_light.html")
+    return render(request, "seriessingle.html")
+
+
+def userFavoriteGrid(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "userfavoritegrid_light.html")
+    return render(request, "userfavoritegrid.html")
+
+
+def userProfile(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "userprofile_light.html")
+    return render(request, "userprofile.html")
+
+
+def userRate(request):
+    theme = request.GET.get('theme', 'dark')
+    if theme == 'light':
+        return render(request, "userrate_light.html")
+    return render(request, "userrate.html")
